21d50bf5556db849555d886e28c16d43586b7376
Kerdo Kurs <kerdokurs@gmail.com>
2021-04-10 20:20:38 +0300

    uus päis

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -3,7 +3,7 @@
   import Navlink from './navlink.svelte';
 </script>
 
-<nav
+<!-- <nav
   class="fixed top-0 flex items-center w-full px-2 md:px-14 bg-gray-900 text-white"
 >
   <Link to="/" class="p-2 mr-4 inline-flex items-center">
@@ -39,4 +39,35 @@
       <Navlink to="/registreeri" text="Registreeri" />
     </div>
   </div>
+</nav> -->
+
+<nav class="pt-8">
+  <div class="flex flex-col">
+    <div class="flex flex-row justify-between w-4/5 mx-auto">
+      <div class="flex flex-col justify-end">
+        <img src="/img/robocode_logo.svg" alt="ROBOCODE logo" width="240" />
+      </div>
+      <div class="flex flex-col justify-end">
+        <div class="flex flex-row gap-4">
+          <button
+            class="bg-transparent text-lg border border-oranz px-12 py-1 rounded-3xl focus:outline-none"
+          >
+            Juhend
+          </button>
+          <button
+            class="bg-transparent text-lg border border-oranz px-12 py-1 rounded-3xl focus:outline-none"
+          >
+            Reeglid
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="w-full mt-4 h-1 bg-gradient-to-r from-tlilla to-oranz" />
 </nav>
+
+<style>
+  nav {
+    background: linear-gradient(180deg, #ffab15db 0%, #ff5c4921 94.06%);
+  }
+</style>
