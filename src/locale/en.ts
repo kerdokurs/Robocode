df049957d1f5adc7f4854f522471368f7b360165
Kerdo Kurs <kerdokurs@gmail.com>
2021-04-23 13:26:54 +0300

    final translation

--- src/locale/en.ts
+++ src/locale/en.ts
@@ -5,17 +5,17 @@ export default {
   },
   index: {
     main: {
-      header: "Pane end proovile Robocode'il!",
+      header: 'Put your skills to the test at Robocode!',
       subheader:
-        'Tule tõesta oma progemisoskusi tuntud võistlusprogrammeerimisüritusel - Robocode! Registreeru koos oma tiimiga!',
+        'Test your programming skills at a well-known programming event Robocode! Register your team today!',
       register: 'Register',
     },
     side: {
       header: 'What is Robocode?',
       paragraphs: [
-        'Robocode on programmeerimismäng, kus mängija ülesandeks on kirjutada lahingutank, mis võistleb lahinguväljal teiste tankide vastu.',
-        'Mängija on roboti programmeerija, kellel ei ole mängu toimumise ajal mingit otsest mõju mängukursile. Mängija peab kirjutama roboti AI, mis määrab roboti käitumise ja reageerimise lahinguväljal toimuvatele sündmustele.',
-        'Võistlemine lahinguväljal toimub reaalajas, kus kuuerattalised automatiseeritud tankid võitlevad niikaua, kuni ainult võitja jääb järele.',
+        'Robocode is a programming game in which the players have the objective to code a battletank that which will fight against other tanks on the battlefield.',
+        'The player is only the programmer - they will not have any direct impact on the course of the battle. The player must write the AI which defines the behaviour and reactions of the tank according to the current events on the battlefield.',
+        'The battle on the battlefield will take place in real time. The programmed tanks will battle until only one of them is left.',
       ],
     },
   },
@@ -72,20 +72,20 @@ export default {
     participating: {
       header: 'Participating',
       rules: [
-        'Võistlusel saab osaleda 1-5-liikmeline võistkond.',
-        'Võistkonnas peab olema vähemalt üks Eesti ülikooli üliõpilane.',
-        'Võistkonna osalemine tuleb registreerida hiljemalt 12. mai 2021.',
-        'Võistkond peab oma eelprogrammeeritud võistlusmasinaga osalema 16.05.2021 virtuaalselt toimuval lõppvõistlusel. Esindamata võistkonnad diskvalifitseeritakse.',
-        'Võistkond peab korraldaja antud ajaks andma korraldaja määratud isikule juurdepääsu koodile. Kood peab läbima plagiaadikontrolli nii teiste võistlejate kui ka levinud teiste võistlusmasinate suhtes. Plagiaadi tuvastamisel tulemused tühistatakse.',
+        'Participating teams must have 1-5 members.',
+        'Each participating team must have at least one member studying in an Estonian university.',
+        'Registration deadline is on the 12th of May 2021.',
+        'The team must participate in the virtual final battle with their pre-programmed machine on the 16th of May 2021. Unrepresented teams will be disqualified.',
+        'Each team must grant access to the code. By what time and to whom will be assigned by the organisers. Each code must pass through a plagiarism control where they will be compared to both other contestants and already well-known code. All plagiarized works will be revoked.',
       ],
     },
     tech: {
       header: 'Technical',
       rules: [
-        'Kood peab olema kirjutatud, kasutades Robocode’i viimast frameworki versiooni <b>1.9.4</b>',
-        'Võistkondade võistlusmasinad peavad võistlema üks ühe vastu lahinguväljal mõõtmetega 800x800 pikslit. Järgmisse vooru pääseb matšis enim lahinguid võitnud võistkond',
-        'Toimub "1vs1" võistlus, kus selgitatakse välja esikolmik.',
-        'Toimub "kõik kõigi vastu", kus viimane ellujääja on võitja.',
+        'The code must be written using the latest framework version of Robocode (<b>1.9.4</b>).',
+        'Coded tanks must compete in a duel on a battleground with the size of 800x800 pixels. The team with most battles won in a match will advance to the next round.',
+        'First three places will be determined in a “1vs1” competition.',
+        'A “free for all” will take place, the last man standing will be announced as the winner.',
       ],
     },
   },
